a23f9d29b53fb236bae7ff30b7152cd9c24d58ad
Lucius <z360394624@163.com>
2016-12-05 01:32:10 +0800

    update http lib

--- HttpLib/src/main/java/com/hgtec/http/bean/HttpParams.java
+++ HttpLib/src/main/java/com/hgtec/http/bean/HttpParams.java
@@ -1,7 +1,29 @@
 package com.hgtec.http.bean;
 
+import java.util.Map;
+
 /**
  * Created by Lucius on 2016/12/4.
  */
 public class HttpParams {
+
+    private Map<String, String> params;
+
+    private Map<String, String> header;
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
+
+    public Map<String, String> getHeader() {
+        return header;
+    }
+
+    public void setHeader(Map<String, String> header) {
+        this.header = header;
+    }
 }
